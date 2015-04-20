34bdf7f7c60ecada102b01b1568527d7a6b75cdc
Laufes <magmich2@gmail.com>
2015-04-21 00:44:20 +0200

    zmiana

--- LargeNumber.h
+++ LargeNumber.h
@@ -23,11 +23,18 @@ private:
 	*/
 	LargeNumber Addition(LargeNumber &x, LargeNumber &y);
 	LargeNumber Subtraction(LargeNumber &x, LargeNumber &y);
+	LargeNumber Multiplication(LargeNumber &x, LargeNumber &y);
+	LargeNumber Division(LargeNumber &x, LargeNumber &y); 
+
+	// Usuwanie niepotrzebnych zero powstalych po dzialaniach arytmetycznych
+	uint8_t* CutZeros(uint8_t *x);
+	uint8_t* CutZerosRight(uint8_t *x, unsigned int &length, const unsigned int &markPoint);
+	uint8_t* CutZerosLeft(uint8_t *x, unsigned int &length, const unsigned int &markPoint);
 public:
 	/*
 	Konstruktory
 	*/
-	// Tworzy 0.0 
+	// Tworzy 0 
 	LargeNumber();
 	LargeNumber(unsigned int length);
 	LargeNumber(LargeNumber number, unsigned int length);
@@ -35,7 +42,7 @@ public:
 	LargeNumber(uint8_t* array, unsigned int length, unsigned int markPoint, bool positive);
 	//Tworzy liczbe ze stringa
 	LargeNumber(string number);
-	
+
 	/*
 	Destruktor
 	*/
@@ -48,9 +55,9 @@ public:
 	unsigned int GetNumberOfDigits();
 	bool GetSign();
 	uint8_t Digit(unsigned int position);
-	
-	friend LargeNumber operator+ ( LargeNumber &x,  LargeNumber &y);
-	friend LargeNumber operator- ( LargeNumber &x,  LargeNumber &y);
-	
+
+	friend LargeNumber operator+ (LargeNumber &x, LargeNumber &y);
+	friend LargeNumber operator- (LargeNumber &x, LargeNumber &y);
+
 };
 
